76670551b1925758b7b1785621b045651a12fc3a
David Gardiner <david@gardiner.net.au>
2025-04-28 14:35:50 +0930

    Firefox prefs, Git and Windows Terminal

--- post-boxstarter-nonadmin.ps1
+++ post-boxstarter-nonadmin.ps1
@@ -2,14 +2,19 @@
 & 'C:\Program Files\Mozilla Firefox\firefox.exe' --headless --screenshot nul
 
 $firefoxProfile = Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles" | Where-Object { $_.Name -match "default-release" } | Select-Object -First 1
-
-# TODO confirm this is the right place to copy prefs.js to
+Copy-Item .\prefs.js -Destination $($firefoxProfile.FullName)\user.js
 
 # Git configuration
 
-git config --global core.editor "code --wait"
-git config --global fetch.prune true
-git config --global push.autoSetupRemote true
-git config --global user.email "david@gardiner.net.au"
-git config --global user.name "David Gardiner"
-git config --global init.defaultbranch "main"
+# git config --global core.editor "code --wait"
+# git config --global fetch.prune true
+# git config --global push.autoSetupRemote true
+# git config --global user.email "david@gardiner.net.au"
+# git config --global user.name "David Gardiner"
+# git config --global init.defaultbranch "main"
+Copy-Item $env:OneDrive\Desktop\.gitconfig* $env:USERPROFILE\
+
+# Windows Terminal
+Copy-Item .\settings.json $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\
+
+
