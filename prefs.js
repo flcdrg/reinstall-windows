7da2c9611938e3ec48ae3a9c83ff9dbbbabba28b
David Gardiner <david@gardiner.net.au>
2025-04-28 14:36:09 +0930

    Firefox don't remember signons

--- prefs.js
+++ prefs.js
@@ -2,3 +2,6 @@
 user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
 user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
 user_pref("browser.download.useDownloadDir", false);
+
+// We use Bitarden to save signons
+user_pref("signon.rememberSignons", false);
