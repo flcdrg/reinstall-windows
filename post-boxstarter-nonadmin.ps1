42bd0b3097707070a6e3158b61c3bff2b18f16f7
David Gardiner <david@gardiner.net.au>
2025-04-29 10:01:06 +0930

    Fix Firefox config
    - Wait for FF to finish before copying prefs
    - Add fnm config

--- post-boxstarter-nonadmin.ps1
+++ post-boxstarter-nonadmin.ps1
@@ -1,8 +1,12 @@
 # Create Firefox profile (so we can then set prefs)
 & 'C:\Program Files\Mozilla Firefox\firefox.exe' --headless --screenshot nul
 
+# The above returns immediately, but it takes a bit to create the directory
+
+Start-Sleep -Seconds 3
+
 $firefoxProfile = Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles" | Where-Object { $_.Name -match "default-release" } | Select-Object -First 1
-Copy-Item .\prefs.js -Destination $($firefoxProfile.FullName)\user.js
+Copy-Item .\prefs.js -Destination "$($firefoxProfile.FullName)\user.js"
 
 # Git configuration
 
@@ -18,3 +22,9 @@ Copy-Item $env:OneDrive\Desktop\.gitconfig* $env:USERPROFILE\
 Copy-Item .\settings.json $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\
 
 
+if (-not(Get-Command node -ErrorAction Ignore)) {
+    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+    
+    fnm install --lts
+    fnm use lts-latest
+}
