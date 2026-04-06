d14b55a5e29774a6aea7966bd916afbde77fc450
David Gardiner <david@gardinerfamily.au>
2026-04-07 08:36:45 +0930

    Update Set-PathVariable

--- Set-PathVariable.ps1
+++ Set-PathVariable.ps1
@@ -1,70 +1,87 @@
 # Based on https://stackoverflow.com/a/39959060/25702, but adapted to change user-level environment variables.
 
-<#
-.SYNOPSIS
-    Add a new location to the user path variable
-.PARAMETER NewLocation
-    The new location to add to the path variable.
-#>
+function Set-PathVariable {
+    <#
+    .SYNOPSIS
+        Add a new location to the user path variable.
+    .PARAMETER NewLocation
+        The new location to add to the path variable.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$NewLocation
+    )
 
-. "$PSScriptRoot\Set-PathVariableHelpers.ps1"
+    begin {
+        function Build-NewPath {
+            <#
+            .SYNOPSIS
+                Builds a new PATH string by appending a new location to an existing PATH value.
+            .PARAMETER OldPath
+                The existing PATH value.
+            .PARAMETER NewLocation
+                The new location to append.
+            #>
+            param(
+                [string]$OldPath,
+                [string]$NewLocation
+            )
 
-[CmdletBinding(SupportsShouldProcess=$true)]
- param(
-     [parameter(Mandatory=$true)]
-     [string]$NewLocation)
+            # Build the new path, make sure we don't have consecutive semicolons or a leading semicolon.
+            $newPath = $OldPath + ";" + $NewLocation
+            $newPath = ($newPath -replace ";+", ";").TrimStart(";")
 
- Begin
- {
-     $regPath = "Environment"
-     $hklm = [Microsoft.Win32.Registry]::CurrentUser
+            return $newPath
+        }
 
-     Function GetOldPath()
-     {
-         $regKey = $hklm.OpenSubKey($regPath, $FALSE)
-         $envpath = $regKey.GetValue("Path", "", [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
-         return $envPath
-     }
- }
+        $regPath = "Environment"
+        $hklm = [Microsoft.Win32.Registry]::CurrentUser
 
- Process
- {
-     # Win32API error codes
-     $ERROR_SUCCESS = 0
-     $ERROR_DUP_NAME = 34
-     $ERROR_INVALID_DATA = 13
+        function GetOldPath {
+            $regKey = $hklm.OpenSubKey($regPath, $false)
+            $envPath = $regKey.GetValue("Path", "", [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+            return $envPath
+        }
+    }
 
-     $NewLocation = $NewLocation.Trim();
+    process {
+        # Win32API error codes
+        $ERROR_SUCCESS = 0
+        $ERROR_DUP_NAME = 34
+        $ERROR_INVALID_DATA = 13
 
-     If ($NewLocation -eq "" -or $NewLocation -eq $null)
-     {
-         Exit $ERROR_INVALID_DATA
-     }
+        $NewLocation = $NewLocation.Trim()
 
-     [string]$oldPath = GetOldPath
-     Write-Verbose "Old Path: $oldPath"
+        if ($NewLocation -eq "" -or $null -eq $NewLocation) {
+            #return $ERROR_INVALID_DATA
+            Write-Warning "The new location is empty or null"
+            return
+        }
 
-     # Check whether the new location is already in the path
-     $parts = $oldPath.split(";")
-     If ($parts -contains $NewLocation)
-     {
-         Write-Warning "The new location is already in the path"
-         Exit $ERROR_DUP_NAME
-     }
+        [string]$oldPath = GetOldPath
+        Write-Verbose "Old Path: $oldPath"
 
-     # Build the new path, make sure we don't have double semicolons
-      $newPath = Build-NewPath -OldPath $oldPath -NewLocation $NewLocation
+        # Check whether the new location is already in the path
+        $parts = $oldPath.Split(";")
+        if ($parts -contains $NewLocation) {
+            Write-Warning "The new location is already in the path"
+            return
+        }
 
-     if ($pscmdlet.ShouldProcess("%Path%", "Add $NewLocation")){
+        # Build the new path, make sure we don't have double semicolons
+        $newPath = Build-NewPath -OldPath $oldPath -NewLocation $NewLocation
 
-         # Add to the current session
-         $env:path += ";$NewLocation"
+        if ($PSCmdlet.ShouldProcess("%Path%", "Add $NewLocation")) {
+            # Add to the current session
+            $env:path += ";$NewLocation"
 
-         # Save into registry
-         $regKey = $hklm.OpenSubKey($regPath, $True)
-         $regKey.SetValue("Path", $newPath, [Microsoft.Win32.RegistryValueKind]::ExpandString)
-         Write-Output "The operation completed successfully."
-     }
+            # Save into registry
+            $regKey = $hklm.OpenSubKey($regPath, $true)
+            $regKey.SetValue("Path", $newPath, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+            Write-Output "The operation completed successfully."
+        }
 
-     Exit $ERROR_SUCCESS
- }
+        return
+    }
+}
--- post-install-nonadmin.ps1
+++ post-install-nonadmin.ps1
@@ -1,6 +1,94 @@
 # This script is embedded in the autounattend.xml as part of the UserOnce script.
 # As such, it must not reference any other files/scripts.
 
+# Based on https://stackoverflow.com/a/39959060/25702, but adapted to change user-level environment variables.
+
+function Set-PathVariable {
+    <#
+    .SYNOPSIS
+        Add a new location to the user path variable.
+    .PARAMETER NewLocation
+        The new location to add to the path variable.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$NewLocation
+    )
+
+    begin {
+        function Build-NewPath {
+            <#
+            .SYNOPSIS
+                Builds a new PATH string by appending a new location to an existing PATH value.
+            .PARAMETER OldPath
+                The existing PATH value.
+            .PARAMETER NewLocation
+                The new location to append.
+            #>
+            param(
+                [string]$OldPath,
+                [string]$NewLocation
+            )
+
+            # Build the new path, make sure we don't have consecutive semicolons or a leading semicolon.
+            $newPath = $OldPath + ";" + $NewLocation
+            $newPath = ($newPath -replace ";+", ";").TrimStart(";")
+
+            return $newPath
+        }
+
+        $regPath = "Environment"
+        $hklm = [Microsoft.Win32.Registry]::CurrentUser
+
+        function GetOldPath {
+            $regKey = $hklm.OpenSubKey($regPath, $false)
+            $envPath = $regKey.GetValue("Path", "", [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+            return $envPath
+        }
+    }
+
+    process {
+        # Win32API error codes
+        $ERROR_SUCCESS = 0
+        $ERROR_DUP_NAME = 34
+        $ERROR_INVALID_DATA = 13
+
+        $NewLocation = $NewLocation.Trim()
+
+        if ($NewLocation -eq "" -or $null -eq $NewLocation) {
+            #return $ERROR_INVALID_DATA
+            Write-Warning "The new location is empty or null"
+            return
+        }
+
+        [string]$oldPath = GetOldPath
+        Write-Verbose "Old Path: $oldPath"
+
+        # Check whether the new location is already in the path
+        $parts = $oldPath.Split(";")
+        if ($parts -contains $NewLocation) {
+            Write-Warning "The new location is already in the path"
+            return
+        }
+
+        # Build the new path, make sure we don't have double semicolons
+        $newPath = Build-NewPath -OldPath $oldPath -NewLocation $NewLocation
+
+        if ($PSCmdlet.ShouldProcess("%Path%", "Add $NewLocation")) {
+            # Add to the current session
+            $env:path += ";$NewLocation"
+
+            # Save into registry
+            $regKey = $hklm.OpenSubKey($regPath, $true)
+            $regKey.SetValue("Path", $newPath, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+            Write-Output "The operation completed successfully."
+        }
+
+        return
+    }
+}
+
 # Things to install/run as the signed-in user, but not elevated
 
 # Install Azure Artifacts credential provider
@@ -14,29 +102,7 @@ Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-artifacts-cred
 [Environment]::SetEnvironmentVariable("PNPM_HOME", "D:\packages\pnpm-store\", [System.EnvironmentVariableTarget]::User)
 
 # Add pnpm global store to PATH (to support global packages)
-# TODO check this isn't corrupting the PATH?
-$newLocation = "D:\packages\pnpm-store\".Trim()
-$regPath = "Environment"
-$hklm = [Microsoft.Win32.Registry]::CurrentUser
-$regKey = $hklm.OpenSubKey($regPath, $false)
-$oldPath = $regKey.GetValue("Path", "", [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
-
-# Add only when missing to avoid duplicate path entries.
-$parts = $oldPath -split ";"
-if ($parts -notcontains $newLocation) {
-    $newPath = ($oldPath + ";" + $newLocation) -replace ";;", ""
-
-    if ([string]::IsNullOrEmpty($env:Path)) {
-        $env:Path = $newLocation
-    }
-    else {
-        $env:Path += ";" + $newLocation
-    }
-
-    $writeKey = $hklm.OpenSubKey($regPath, $true)
-    $writeKey.SetValue("Path", $newPath, [Microsoft.Win32.RegistryValueKind]::ExpandString)
-}
-
+Set-PathVariable -NewLocation "D:\packages\pnpm-store"
 
 # NPM cache
 [Environment]::SetEnvironmentVariable("npm_config_cache", "D:\packages\npm-cache\", [System.EnvironmentVariableTarget]::User)
@@ -64,6 +130,15 @@ if ($parts -notcontains $newLocation) {
 
 # Vagrant Home - https://developer.hashicorp.com/vagrant/docs/other/environmental-variables#vagrant_home
 [Environment]::SetEnvironmentVariable("VAGRANT_HOME", "D:\packages\vagrant.d", [System.EnvironmentVariableTarget]::User)
+
+# Android development
+if (-not (Test-Path "D:\packages\Android\Sdk")) {
+    New-Item -ItemType Directory -Path "D:\packages\Android\Sdk" | Out-Null
+}
+[Environment]::SetEnvironmentVariable("ANDROID_HOME", "D:\packages\Android\Sdk", [System.EnvironmentVariableTarget]::User)
+
+Set-PathVariable -NewLocation "%ANDROID_HOME%\platform-tools"
+Set-PathVariable -NewLocation "%ANDROID_HOME%\emulator"
 
 # Tell Corepack not to automatically add packageManager to package.json files (https://blog.hyperknot.com/p/corepacks-packagemanager-field)
 [Environment]::SetEnvironmentVariable("COREPACK_ENABLE_AUTO_PIN", "0", [System.EnvironmentVariableTarget]::User)
